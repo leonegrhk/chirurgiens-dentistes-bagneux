591581bd9704c00b3f05bbbee3112e5a6f000383
leonegrhk <lea.rochagameira@gmail.com>
2025-11-19 19:24:02 +0100

    Changement couleur exercice individuel

--- styles/Chirurgiensdentistesindividuels_2_style.js
+++ styles/Chirurgiensdentistesindividuels_2_style.js
@@ -22,7 +22,7 @@ var style_Chirurgiensdentistesindividuels_2 = function(feature, resolution){
     }
     var style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 6.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(128,227,161,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(128,227,161,1.0)'})}),
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,0,180,1)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(0,0,180,1)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
